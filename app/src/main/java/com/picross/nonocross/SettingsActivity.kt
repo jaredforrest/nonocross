ad56cee2746380fdc754cdafef72757b2a7b2972
mueller-ma <mueller-ma@users.noreply.github.com>
2023-05-20 08:40:34 +0200

    Add about menu
    It's reachable via the settings and shows the version, used libs and has links to GitHub and F-Droid.
    
    Closes #49

--- app/src/main/java/com/picross/nonocross/SettingsActivity.kt
+++ app/src/main/java/com/picross/nonocross/SettingsActivity.kt
@@ -1,8 +1,16 @@
 package com.picross.nonocross
 
 import android.os.Bundle
+import android.view.View
 import androidx.appcompat.app.AppCompatActivity
+import androidx.fragment.app.commit
+import androidx.preference.Preference
 import androidx.preference.PreferenceFragmentCompat
+import com.mikepenz.aboutlibraries.LibsBuilder
+import com.mikepenz.aboutlibraries.LibsConfiguration
+import com.mikepenz.aboutlibraries.entity.Library
+import com.mikepenz.aboutlibraries.util.SpecialButton
+import com.picross.nonocross.util.openInBrowser
 
 class SettingsActivity : AppCompatActivity() {
 
@@ -21,6 +29,66 @@ class SettingsActivity : AppCompatActivity() {
     class SettingsFragment : PreferenceFragmentCompat() {
         override fun onCreatePreferences(savedInstanceState: Bundle?, rootKey: String?) {
             setPreferencesFromResource(R.xml.root_preferences, rootKey)
+
+            val aboutPref = preferenceManager.findPreference<Preference>("about")!!
+            aboutPref.setOnPreferenceClickListener {
+                val fragment = LibsBuilder()
+                    .withAboutIconShown(true)
+                    .withAboutVersionShownName(true)
+                    .withSortEnabled(true)
+                    .withListener(AboutButtonsListener())
+                    .supportFragment()
+
+                parentFragmentManager.commit {
+                    addToBackStack("about")
+                    replace(R.id.settings, fragment, "about")
+                }
+                true
+            }
         }
+    }
+}
+
+class AboutButtonsListener : LibsConfiguration.LibsListener {
+    override fun onExtraClicked(v: View, specialButton: SpecialButton): Boolean {
+        val link = when (specialButton) {
+            SpecialButton.SPECIAL1 -> "https://github.com/jaredforrest/nonocross/"
+            SpecialButton.SPECIAL2 -> "https://f-droid.org/packages/com.picross.nonocross/"
+            SpecialButton.SPECIAL3 -> null
+        }
+        link?.openInBrowser(v.context)
+        return true
+    }
+
+    override fun onIconClicked(v: View) {
+        // no-op
+    }
+
+    override fun onIconLongClicked(v: View): Boolean {
+        return false
+    }
+
+    override fun onLibraryAuthorClicked(v: View, library: Library): Boolean {
+        return false
+    }
+
+    override fun onLibraryAuthorLongClicked(v: View, library: Library): Boolean {
+        return false
+    }
+
+    override fun onLibraryBottomClicked(v: View, library: Library): Boolean {
+        return false
+    }
+
+    override fun onLibraryBottomLongClicked(v: View, library: Library): Boolean {
+        return false
+    }
+
+    override fun onLibraryContentClicked(v: View, library: Library): Boolean {
+        return false
+    }
+
+    override fun onLibraryContentLongClicked(v: View, library: Library): Boolean {
+        return false
     }
 }
